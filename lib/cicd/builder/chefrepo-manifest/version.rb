383abdbeec3c1c91109c73b0e22239a438ce99b9
Christo DeLange <github@dldinternet.com>
2015-04-28 16:06:36 -0400

    Create a classes.yaml file for deployments

--- lib/cicd/builder/chefrepo-manifest/version.rb
+++ lib/cicd/builder/chefrepo-manifest/version.rb
@@ -2,7 +2,7 @@ module CiCd
   module Builder
     module ChefRepoManifest
       # chefrepo-manifest-builder version
-      VERSION = '0.1.0'
+      VERSION = '0.1.1'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
