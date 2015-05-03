64b25d4295b163599cdd5fd1d37c3e449119b74b
Christo DeLange <github@dldinternet.com>
2015-05-03 00:15:24 -0400

    build.branch property follows explicit settings and vcs.branch will indicate the git branch

--- lib/cicd/builder/chefrepo-manifest/version.rb
+++ lib/cicd/builder/chefrepo-manifest/version.rb
@@ -2,7 +2,7 @@ module CiCd
   module Builder
     module ChefRepoManifest
       # chefrepo-manifest-builder version
-      VERSION = '0.1.5'
+      VERSION = '0.1.6'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
